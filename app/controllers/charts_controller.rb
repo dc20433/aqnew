345668a41c5609c03cd0e9d8e43494e9ab0c7a5e
Jj <dc20433@gmail.com>
2022-11-26 22:13:28 -0500

    Fixed Chart pages

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,50 +1,47 @@
 class ChartsController < ApplicationController
   before_action :set_regi
-  before_action :set_chart, only: [:show, :edit, :update, :destroy]
+  before_action :set_chart, only: [:show, :edit, :update, :destroy] 
 
   # GET regis/1/charts
   def index
-    @charts = @regi.charts
-  end
-
-  # GET regis/1/charts/1
-  def show
   end
 
   # GET regis/1/charts/new
   def new
-    @chart = @regi.charts.build
+    @chart = Chart.new
+  end
+
+  # POST regis/1/charts
+  def create
+    @chart = Chart.new chart_params
+    if @regi.charts << @chart
+      redirect_to regi_charts_path(@regi,@chart), notice: 'Patient Chart created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  # GET regis/1/charts/1
+  def show
   end
 
   # GET regis/1/charts/1/edit
   def edit
   end
 
-  # POST regis/1/charts
-  def create
-    @chart = @regi.charts.build(chart_params)
-
-    if @chart.save
-      redirect_to([@chart.regi, @chart], notice: 'Chart was successfully created.')
-    else
-      render action: 'new'
-    end
-  end
-
   # PUT regis/1/charts/1
   def update
-    if @chart.update_attributes(chart_params)
-      redirect_to([@chart.regi, @chart], notice: 'Chart was successfully updated.')
+    if @chart.update chart_params
+      redirect_to(regi_charts_path(@regi), notice: 'Patient Chart updated.')
     else
-      render action: 'edit'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE regis/1/charts/1
   def destroy
     @chart.destroy
-
-    redirect_to regi_charts_url(@regi)
+    redirect_to regi_charts_path(@regi), alert: "Patient Chart deleted."
   end
 
   private
@@ -59,6 +56,6 @@ class ChartsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def chart_params
-      params.require(:chart).permit(:tDate, :soap, :acuList, :cMedList, :cForList, :oTreats, :regi_id)
+      params.require(:chart).permit(:tDate, :soap, :acuList, :cMedList, :cForList, :oTreats, :regi_id, :id)
     end
 end
